755df8f624d8f91db5a7949ca0e6dceb335494b0
TimurBazarov <74816879+TimurBazarov@users.noreply.github.com>
2023-01-01 17:10:24 +0300

    Новая механика игры. Присутствует камера.

--- programm.py
+++ programm.py
@@ -1,13 +1,12 @@
 import pygame
-import os
 import sys
+import os
+from random import choice
 
 
-FPS, WIDTH, HEIGHT = 50, 640, 640
-tile_width = tile_height = 32
-tile_size = tile_width, tile_height
+FPS = 50
 pygame.init()
-size = width, height = WIDTH, HEIGHT
+size = WIDTH, HEIGHT = 550, 550
 screen = pygame.display.set_mode(size)
 
 
@@ -17,9 +16,24 @@ def terminate():
 
 
 def start_screen():
+    intro_text = ['Перемещение героя',
+                  '',
+                  'Камера']
+
     fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
     screen.blit(fon, (0, 0))
     clock = pygame.time.Clock()
+
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
 
     while True:
         for event in pygame.event.get():
@@ -27,13 +41,13 @@ def start_screen():
                 terminate()
             elif event.type == pygame.KEYDOWN or \
                     event.type == pygame.MOUSEBUTTONDOWN:
-                return  # начинаем игру
+                return
         pygame.display.flip()
         clock.tick(FPS)
 
 
 def load_level(filename):
-    filename = "data/" + filename
+    filename = os.path.join('data', filename)
     with open(filename, 'r') as mapFile:
         level_map = [line.strip() for line in mapFile]
     max_width = max(map(len, level_map))
@@ -57,99 +71,61 @@ def load_image(name, colorkey=None):
 
 
 tile_images = {
-    '.': load_image('Tile_12.png'),
-    'T': load_image('Tile_01.png'),
-    '-': load_image('Tile_33.png'),
-    '|': load_image('Tile_55.png'),
-    'C': load_image('Tile_32.png'),
-    '7': load_image('Tile_03.png'),
-    'L': load_image('Tile_21.png'),
-    'A': load_image('Tile_54.png'),
-    'J': load_image('Tile_23.png'),
-    't': load_image('Tile_11.png'),
-    'D': load_image('Tile_34.png'),
-    'U': load_image('Tile_56.png')
+    'wall': load_image('box.png'),
+    'empty': load_image('grass.png')
 }
-'''Символы стен. Например, Символ Т означает, что блок -  стена, у которой справа, слева и внизу
- есть продолжения. Символ 7 означает правый верхний угол. Символ D означает конец стены, которая шла слева направо.
- Дальше по аналогии.'''
-wall_tiles = ['T', '-', '|', 'C', '7', 'L', 'A', 'J', 't', 'D', 'U']
-#  . @ T - | C 7 L A J t D U - все использующиеся символы
 player_image = load_image('mario.png')
+
+tile_width = tile_height = 50
 
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self, tile_type, pos_x, pos_y):
-        if tile_type == '.':
-            super().__init__(ground_group, all_sprites)
+        if tile_type == 'empty':
+            super().__init__(tiles_group, all_sprites)
         else:
-            super().__init__(walls_group, all_sprites)
+            super().__init__(tiles_group, walls_group, all_sprites)
         self.image = tile_images[tile_type]
-        self.image = pygame.transform.scale(self.image, tile_size)
         self.rect = self.image.get_rect().move(
             tile_width * pos_x, tile_height * pos_y)
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
-        super().__init__(player_group)
-        self.mask = pygame.mask.from_surface(player_image)
+        super().__init__(player_group, all_sprites)
         self.image = player_image
-        self.image = pygame.transform.scale(self.image, tile_size)
-        self.rect = self.image.get_rect().move(pos_x, pos_y)
-        self.x, self.y = pos_x, pos_y
-
-    def find_current_tile(self):
-        pass
-
-    def move(self, dx, dy):
-        self.rect = self.rect.move(dx, dy)
-
-    def return_x_y(self):
-        return self.x, self.y
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 15, tile_height * pos_y + 5)
 
 
 player = None
 
 all_sprites = pygame.sprite.Group()
 walls_group = pygame.sprite.Group()
-ground_group = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
 
 
 def generate_level(level):
+    all_sprites.empty()
     new_player, x, y = None, None, None
     for y in range(len(level)):
         for x in range(len(level[y])):
             if level[y][x] == '.':
-                Tile('.', x, y)
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
             elif level[y][x] == '@':
-                Tile('.', x, y)
-                new_player = Player(x * tile_width - 320, y * tile_height - 320)
-            elif level[y][x] == 'T':
-                Tile('T', x, y)
-            elif level[y][x] == '-':
-                Tile('-', x, y)
-            elif level[y][x] == '|':
-                Tile('|', x, y)
-            elif level[y][x] == 'C':
-                Tile('C', x, y)
-            elif level[y][x] == '7':
-                Tile('7', x, y)
-            elif level[y][x] == 'L':
-                Tile('L', x, y)
-            elif level[y][x] == 'A':
-                Tile('A', x, y)
-            elif level[y][x] == 'J':
-                Tile('J', x, y)
-            elif level[y][x] == 't':
-                Tile('t', x, y)
-            elif level[y][x] == 'D':
-                Tile('D', x, y)
-            elif level[y][x] == 'U':
-                Tile('U', x, y)
-    # вернем игрока, а также размер поля в клетках
-    return new_player, x + 1, y + 1
+                Tile('empty', x, y)
+                new_player = Player(x, y)
+    return new_player, x, y
+
+
+def find_player(level):
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '@':
+                return y, x
 
 
 class Camera:
@@ -159,7 +135,12 @@ class Camera:
             self.dy = -320
         else:
             pass
-        self.level_num = level_num
+        self.dx = 0
+        self.dy = 0
+
+    def apply(self, obj):
+        obj.rect.x += self.dx
+        obj.rect.y += self.dy
 
     def update(self, x, y):
         self.dx -= x
@@ -171,16 +152,21 @@ class Camera:
 
 if __name__ == '__main__':
     start_screen()
-    pygame.display.set_caption('Mario')
-    level_num = 1  # level_num будет увеличиваться с переходом на новый уровень
-    level = load_level(f'{level_num}.txt')
+    screen.fill('black')
+    pygame.display.set_caption('Game')
+    level_num = choice(range(1, 6))
+    level_size = choice(['small', 'large'])
+    level = load_level(f'{level_size}/level{level_num}.txt')
 
-    running = True
-    to_move_up, to_move_down, to_move_right, to_move_left = False, False, False, False
-    player, level_w, level_h = generate_level(level)
     camera = Camera(level_num)
-    surf = pygame.Surface((level_w * tile_width, level_h * tile_height))
-    all_sprites.draw(surf)
+    player, level_x, level_y = generate_level(level)
+    all_sprites.draw(screen)
+    pygame.display.flip()
+
+    pygame.event.set_allowed([pygame.QUIT, pygame.KEYDOWN, pygame.KEYUP])
+
+    to_move_up, to_move_down, to_move_right, to_move_left = False, False, False, False
+    running = True
     step = 5
     while running:
         dx, dy = 0, 0
@@ -188,16 +174,17 @@ if __name__ == '__main__':
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYDOWN:
-                '''Движение происходит, если плитка, в которую хочет перейти персонаж, не является стеной, и если
-                    персонаж не выходит за рамки уровня.'''
-                if event.key == pygame.K_s:
-                    to_move_down = True
-                if event.key == pygame.K_w:
-                    to_move_up = True
-                if event.key == pygame.K_d:
-                    to_move_right = True
-                if event.key == pygame.K_a:
-                    to_move_left = True
+                if event.type == pygame.KEYDOWN:
+                    '''Движение происходит, если плитка, в которую хочет перейти персонаж, не является стеной, и если
+                        персонаж не выходит за рамки уровня.'''
+                    if event.key == pygame.K_s:
+                        to_move_down = True
+                    if event.key == pygame.K_w:
+                        to_move_up = True
+                    if event.key == pygame.K_d:
+                        to_move_right = True
+                    if event.key == pygame.K_a:
+                        to_move_left = True
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_s:
                     to_move_down = False
@@ -207,18 +194,20 @@ if __name__ == '__main__':
                     to_move_right = False
                 elif event.key == pygame.K_a:
                     to_move_left = False
-            if to_move_down:
-                dy += step
-            elif to_move_up:
-                dy -= step
-            elif to_move_right:
-                dx += step
-            elif to_move_left:
-                dx -= step
+        if to_move_down:
+            dy += step
+        elif to_move_up:
+            dy -= step
+        elif to_move_right:
+            dx += step
+        elif to_move_left:
+            dx -= step
         screen.fill('black')
         moved_player, moved_x, moved_y = generate_level(level)
         camera.update(dx, dy)
         ds = camera.return_d()
-        screen.blit(surf, ds)
+        for sprite in tiles_group:
+            camera.apply(sprite)
+        all_sprites.draw(screen)
         player_group.draw(screen)
         pygame.display.flip()
